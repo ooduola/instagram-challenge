d1aaae6527fd28f9ebfab066dff8d38391fce547
Olatunji Oduola <ola.oduola@gmail.com>
2020-07-06 22:09:37 +0100

    Updated db from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_06_021355) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "caption"
     t.datetime "created_at", precision: 6, null: false
