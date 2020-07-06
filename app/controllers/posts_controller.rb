3094c442deddff32a8d79c4e01427f953a702764
Olatunji Oduola <ola.oduola@gmail.com>
2020-07-06 23:38:34 +0100

    Set up Active Storage and added create route to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,17 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.create(post_params)
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption, :image)
+  end
+
 end
 
